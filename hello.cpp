51f1ef61119c008f2e6f94f345af0b7716f5a550
William Miller <wmilleiv@gmail.com>
2026-09-02 14:18:27 -0400

    first commit

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(){
+
+	std::cout << "What is your name?\n";
+	std::string name{};
+	std::cin >> name;
+	std::cout << "Hello " + name + '\n';
+
+	return 0;
+
+}
